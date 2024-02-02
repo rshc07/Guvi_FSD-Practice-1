7ee899160cd52c5f3d73796c407251dc7095964a
Hemachandran RS <154044861+rshc07@users.noreply.github.com>
2024-02-02 21:36:48 +0530

    First Commit
    Java program created to check  the number positive or negative

---
+++ NumberChecker.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        } else if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
